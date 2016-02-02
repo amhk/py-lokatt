7cd6d111983f842dc66c453960a2fd17d037982e
Mårten Kongstad <marten.kongstad@gmail.com>
2016-02-02 20:44:22 +0100

    Support reading logcat stream from file
    In addition to reading from a live adb connection, teach adb.Device to
    read from a file. This is mostly useful for development and testing.
    
    Logcat files can be prepared by executing
    
        $ adb exec-out logcat -B >file
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- src/adb.py
+++ src/adb.py
@@ -1,3 +1,4 @@
+import os
 import select
 import shlex
 import struct
@@ -16,12 +17,21 @@ class LogcatEntry(object):
 
 
 class Device(object):
+    def __init__(self, arg=''):
+        if len(arg) > 0 and arg[0] != '-':  # file path
+            if not os.access(arg, os.R_OK):
+                raise Exception('cannot open file for reading: {}'.format(arg))
+            self._cmd = 'cat {}'.format(arg)
+        else:
+            # -B instead of --binary, since the latter is a recent alias of -B
+            self._cmd = 'adb {} exec-out "logcat -B 2>/dev/null"'.format(arg)
+
     def entries(self):
-        # -B instead of --binary, since the latter is a recent alias of -B
-        cmd = 'adb exec-out "logcat -B 2>/dev/null"'
-        proc = subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE)
+        proc = subprocess.Popen(shlex.split(self._cmd), stdout=subprocess.PIPE)
         while select.select([proc.stdout], [], []):
             x = proc.stdout.read(2 * 2)
+            if len(x) == 0:  # end of stream
+                return
             payload_size, header_size = struct.unpack('<HH', x)
 
             if header_size == 0:  # logger_entry_v1
