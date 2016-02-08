04f99eb065f6ac4ba99515fef7dc9638b185ce3c
Mårten Kongstad <marten.kongstad@gmail.com>
2016-02-08 20:02:39 +0100

    Add statusbar and statusbar window classes
    The statusbar tracks information about the currently selected buffer,
    such as its name and number of lines. In the future, the statusbar will
    also accept user input (like ':' in less and vim).
    
    The statusbar window is the curses part of a statusbar. The window
    occupies the last line of the screen.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- src/ctrl.py
+++ src/ctrl.py
@@ -1,6 +1,6 @@
 from Queue import PriorityQueue
 
-from ui import BufferWindow
+from ui import BufferWindow, StatusbarWindow
 from ui import refresh as refresh_ui
 
 
@@ -23,6 +23,25 @@ class Buffer(object):
         self._entries.append(entry)
         self._window.add_logcat_entry(entry)
 
+    def get_number_of_entries(self):
+        return len(self._entries)
+
+
+class Statusbar(object):
+    def __init__(self, window):
+        self._window = window
+        self._n = 0
+        self._refresh_window()
+
+    def _refresh_window(self):
+        self._window.accept(self._n)
+
+    def accept(self, buf):
+        n = buf.get_number_of_entries()
+        if n != self._n:
+            self._n = n
+            self._refresh_window()
+
 
 def create_context(root_window):
     ctx = Context()
@@ -36,11 +55,15 @@ def main_loop(ctx):
     win = BufferWindow(ctx.root_window)
     buf = Buffer(win)
 
+    win = StatusbarWindow(ctx.root_window)
+    statusbar = Statusbar(win)
+
     while not ctx.done:
         type_, data = ctx.queue.get()
 
         if type_ == EVENT_LOGCAT:
             buf.accept(data)
+            statusbar.accept(buf)
 
         if type_ == EVENT_KEYPRESS:
             if data == ord('q'):
--- src/ui.py
+++ src/ui.py
@@ -4,22 +4,60 @@ import curses
 class BufferWindow(object):
     def __init__(self, root_window):
         self._root_window = root_window
-        self._window = root_window.subwin(0, 0)
+        rh, rw = root_window.getmaxyx()
+
+        self._window = root_window.subwin(rh - StatusbarWindow.HEIGHT, rw, 0, 0)
         self._window.syncok(0)
         self._window.immedok(0)
         self._window.scrollok(1)
         self._window.idlok(1)
+        self._window.leaveok(1)
 
-        self._maxy, self._maxx = self._window.getmaxyx()
+        self._y = 0
+        self._height, self._width = self._window.getmaxyx()
 
     def _add_entry(self, entry):
-        self._window.addnstr('{:20s} {}\n'.format(entry.tag[:20], entry.text), self._maxx)
+        if self._y >= self._height - 1:
+            self._window.scroll(1)
+
+        text = entry.text
+        tag = entry.tag[:20]
+        self._window.insnstr(self._y, 0, '{:20s} {}'.format(tag, text), self._width)
+
+        if self._y < self._height - 1:
+            self._y += 1
 
     def _schedule_refresh(self):
         self._window.noutrefresh()
 
     def add_logcat_entry(self, entry):
         self._add_entry(entry)
+        self._schedule_refresh()
+
+
+class StatusbarWindow(object):
+    HEIGHT = 1
+
+    def __init__(self, root_window):
+        curses.curs_set(0)
+
+        self._root_window = root_window
+        rh, rw = root_window.getmaxyx()
+
+        self._window = root_window.subwin(StatusbarWindow.HEIGHT, rw, rh - StatusbarWindow.HEIGHT, 0)
+        self._window.syncok(0)
+        self._window.immedok(0)
+        self._window.scrollok(0)
+        self._window.leaveok(1)
+
+        self._height, self._width = self._window.getmaxyx()
+
+    def _schedule_refresh(self):
+        self._window.noutrefresh()
+
+    def accept(self, n):
+        self._window.erase()
+        self._window.insnstr('[lokatt] {}{}'.format(n, ' ' * self._width), self._width, curses.A_BOLD | curses.A_REVERSE)
         self._schedule_refresh()
 
 
