590f7facfa8de32ba75ddd8a7364703465adee4a
Mårten Kongstad <marten.kongstad@gmail.com>
2016-02-16 12:56:07 +0100

    scripts: allow user to limit which tests to run
    Teach tests.sh to accept an optional argument which, if given, limits
    what tests will be executed. Without the argument, all tests are
    executed.
    
    Signed-off-by: Mårten Kongstad <marten.kongstad@gmail.com>

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,5 +4,6 @@ script="$(readlink -f $0)"
 root="$(dirname "$script")/.."
 
 PYTHONPATH="$root/src" python -m unittest discover \
+    --pattern="test_$1*.py" \
 	--top-level-directory "$root" \
 	--start-directory "$root/tests"
